4d4bde338284f13bb6741fc779d00a06dbd4af50
sapWRLD <Tomkerstens27@gmail.com>
2025-08-05 12:13:31 +0200

    update for rgb finder

--- RGB_finder/Rgb_Finder.py
+++ RGB_finder/Rgb_Finder.py
@@ -2,9 +2,9 @@ import pyautogui as pag
 import keyboard as kb
 import time
 
+#Convert RGB tuple to HEX string.
 def rgb_to_hex(rgb):
-    """Convert RGB tuple to HEX string."""
-    return '#{:02x}{:02x}{:02x}'.format(*rgb)
+     return '#{:02x}{:02x}{:02x}'.format(*rgb)
 
 def get_color():
     print("Press SPACE to capture the current pixel color or ESC to exit.")
@@ -13,7 +13,7 @@ def get_color():
         if kb.is_pressed('space'):
             x, y = pag.position()
             rgb = pag.pixel(x, y)
-            print(f"Captured color: {rgb} with HAX {rgb_to_hex(rgb)} at position ({x}, {y})") 
+            print(f"Captured color: {rgb} with HEX {rgb_to_hex(rgb)} at position ({x}, {y})") 
             last_rgb = rgb
             time.sleep(0.5)
             
@@ -23,7 +23,7 @@ def get_color():
             return last_rgb
 
 if __name__ == "__main__":
-    rgb = get_color()
+    rgb = get_color()      
     hex = rgb_to_hex(rgb)
     if rgb:
         print(f"Final captured color: {rgb} with HEX value {hex}")       
