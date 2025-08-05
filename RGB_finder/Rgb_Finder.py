c4253f3ebc5be7b0fe3e466602b3c3ac5bacde42
sapWRLD <Tomkerstens27@gmail.com>
2025-08-05 12:09:37 +0200

    added HEX value to rgb finder

--- RGB_finder/Rgb_Finder.py
+++ RGB_finder/Rgb_Finder.py
@@ -1,23 +1,32 @@
 import pyautogui as pag
 import keyboard as kb
-import time 
+import time
+
+def rgb_to_hex(rgb):
+    """Convert RGB tuple to HEX string."""
+    return '#{:02x}{:02x}{:02x}'.format(*rgb)
 
 def get_color():
-    target_color = None
-
     print("Press SPACE to capture the current pixel color or ESC to exit.")
-    while True:
-            if kb.is_pressed('space'):
-                x, y = pag.position()
-                target_color = pag.pixel(x, y)
-                print(f"Captured color: {target_color} at position ({x}, {y})")
-                time.sleep(0.5)    
-            if kb.is_pressed('esc'):
-                print("Exiting...")
-                time.sleep(0.5) 
-                break
-    print(f"Final captured color: {target_color}")
- 
-if __name__ == "__main__": 
-    get_color()                     
-    
+    last_rgb = None
+    while True:     
+        if kb.is_pressed('space'):
+            x, y = pag.position()
+            rgb = pag.pixel(x, y)
+            print(f"Captured color: {rgb} with HAX {rgb_to_hex(rgb)} at position ({x}, {y})") 
+            last_rgb = rgb
+            time.sleep(0.5)
+            
+        if kb.is_pressed('esc'):
+            print("Exiting...")
+            time.sleep(0.5)
+            return last_rgb
+
+if __name__ == "__main__":
+    rgb = get_color()
+    hex = rgb_to_hex(rgb)
+    if rgb:
+        print(f"Final captured color: {rgb} with HEX value {hex}")       
+    else:
+        print("No color captured.")
+   
